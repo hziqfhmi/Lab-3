69fe705a996bfe89482b2b72f4507f848d752a1a
Haziq Fahmi <haziq3935@gmail.com>
2020-11-20 06:18:42 +0000

    Pipe and Fork

---
+++ signal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+int main(void)	{
+
+		void sigint_handler (int sig);
+		char s[200];
+
+		if (signal(SIGINT, sigint_handler) == SIG_ERR)
+		{
+
+		perror ("signal");
+		exit(1);
+
+		}
+
+		printf("Enter stringg: \n");
+		
+			if(fgets(s,200,stdin) ==NULL)
+			perror ("gets");
+
+			else
+
+			printf("awak masuk: %s\n", s);
+
+		return 0;
+
+}
+
+		void sigint_handler (int sig)
+{
+		printf("Jangan Kacau Saya !!!! \n");
+}
